12495efcd725faa1ca836ca4173266534fe7de9e
sandozyv <yvan.sandoz@hotmail.com>
2020-11-17 22:56:45 +0100

    mélange changement yvan/elodie

--- covinvaders.py
+++ covinvaders.py
@@ -24,8 +24,8 @@ pygame.display.set_caption("Convinvaders : Revenge of The Pangolin")
 #Charge l'image des virus
 redVirusImage = pygame.transform.scale(pygame.image.load(os.path.join('covinv_docs/red_virus.png')),(50,50))
 greenVirusImage = pygame.transform.scale(pygame.image.load(os.path.join('covinv_docs/green_virus.png')),(50,50))
-blueVirusImage = pygame.image.load('covinv_docs/blue_virus.png')
-purpleVirusImage = pygame.image.load('covinv_docs/purple_virus.png')
+blueVirusImage = pygame.transform.scale(pygame.image.load(os.path.join('covinv_docs/blue_virus.png')),(50,50))
+purpleVirusImage = pygame.transform.scale(pygame.image.load(os.path.join('covinv_docs/purple_virus.png')),(50,50))
 #test
 test = redVirusImage.get_rect()
 
@@ -75,66 +75,95 @@ StartBGImage =  pygame.transform.scale(pygame.image.load(os.path.join('covinv_do
 #Story_SevenBGImage =  pygame.transform.scale(pygame.image.load(os.path.join('covinv_docs/pngegg.png')),(WINDOW_WIDTH,WINDOW_HEIGHT))
 
 class Virus:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.health = None
-        self.virus_img = redVirusImage
-    def draw(self, window):
-        WINDOW.blit(greenVirusImage,(self.x, self.y))
+   def __init__(self, x, y):
+       self.x = x
+       self.y = y
+       self.health = None
+       self.virus_img = None
+   def draw(self, window):
+       WINDOW.blit(self.virus_img,(self.x, self.y))
+   def update(self):
+       pygame.event.pump()
 
-    def update(self):
-        pygame.event.pump()
-#class Player(virus):
-    #def __init__(self,x ,y):
-        #super().__init__(x,y,)
-        #self.virus_img = redVirusImage
+class Character(Virus):
+   def __init__(self,x ,y):
+       super().__init__(x,y)
+       self.virus_img = redVirusImage
+       self.laser_img = None
 
+class Colorvirus(Virus):
+   Virus_MAP = {
+               "red" : (redVirusImage),
+               "green" : (greenVirusImage ),
+               "blue" : (blueVirusImage ),
+               "purple" : (purpleVirusImage)
+               }
+
+   def __init__(self,x ,y, color):
+       super().__init__(x,y)
+       self.virus_img = self.Virus_MAP[color]
+   def move(self, vel):
+       self.y += vel
 def main():
-    run = True
-    FPS = 60
-    level = 1
-    lives = 3
-    main_font = pygame.font.SysFont("timesnewroman", 20)
+   run = True
+   FPS = 60
+   level = 1
+   lives = 3
+   main_font = pygame.font.SysFont("timesnewroman", 20)
+   enemies = []
+   wave_length = 30
+   virus_vel = 4
 
-    virus = Virus(300, 500)
+   character = Character(300, 500)
 
-    clock = pygame.time.Clock()
+   clock = pygame.time.Clock()
 
-    def redraw_window():
-        WINDOW.blit(StartBGImage, (0,0))
-        WINDOW.blit(BatbossImage, (150,0))
-        #draw text
-        lives_label = main_font.render(f"Lives: {lives}", 1, (255, 0, 255))
-        level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
+   def redraw_window():
+       WINDOW.blit(StartBGImage, (0,0))
+       WINDOW.blit(BatbossImage, (150,0))
+       #draw text
+       lives_label = main_font.render(f"Lives: {lives}", 1, (255, 0, 255))
+       level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
 
-        WINDOW.blit(lives_label, (10, 10))
-        WINDOW.blit(level_label, (WINDOW_WIDTH - level_label.get_width() - 10, 10))
+       WINDOW.blit(lives_label, (10, 10))
+       WINDOW.blit(level_label, (WINDOW_WIDTH - level_label.get_width() - 10, 10))
 
-        virus.draw(WINDOW)
+       for enemy in enemies:
+           enemy.draw(WINDOW)
 
-        pygame.display.update()
-    while run:
-        clock.tick(FPS)
-        redraw_window()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    run = False
+       character.draw(WINDOW)
 
 
 
-        keys = pygame.key.get_pressed()
-        if  keys[pygame.K_LEFT] and virus.x - 5 > 0:
-            virus.x -= 5
-        if keys[pygame.K_RIGHT] and virus.x + 5 + virus.virus_img.get_width() < WINDOW_WIDTH:
-            virus.x += 5
-        if keys[pygame.K_UP] and virus.y - 5 > 0:
-            virus.y -= 5
-        if keys[pygame.K_DOWN] and virus.y + 5 + virus.virus_img.get_height() < WINDOW_HEIGHT:
-            virus.y += 5
+       pygame.display.update()
+   while run:
+       clock.tick(FPS)
+       if len(enemies) == 0:
+           for i in range(wave_length):
+               enemy = Colorvirus(random.randrange(50, WINDOW_WIDTH-100), random.randrange(-1000,-100), random.choice(["red", "blue", "green", "purple"]))
+               enemies.append(enemy)
+       for event in pygame.event.get():
+           if event.type == pygame.QUIT:
+               run = False
+           if event.type == KEYDOWN:
+               if event.key == K_ESCAPE:
+                   run = False
+
+
+
+       keys = pygame.key.get_pressed()
+       if  keys[pygame.K_LEFT] and character.x - 5 > 0:
+           character.x -= 5
+       if keys[pygame.K_RIGHT] and character.x + 5 + character.virus_img.get_width() < WINDOW_WIDTH:
+           character.x += 5
+       if keys[pygame.K_UP] and character.y - 5 > 0:
+           character.y -= 5
+       if keys[pygame.K_DOWN] and character.y + 5 + character.virus_img.get_height() < WINDOW_HEIGHT:
+           character.y += 5
+
+       for enemy in enemies:
+           enemy.move(virus_vel)
+       redraw_window()
 
 main()
 
