dc1d8bda20b2cd064dfc4fa62a18f07a787ccde4
Nemko444 <74020163+Nemko444@users.noreply.github.com>
2020-11-23 01:19:58 +0100

    Update covinvaders.py
    Modification class

--- covinvaders.py
+++ covinvaders.py
@@ -44,7 +44,7 @@ heroImage = pygame.transform.scale(pygame.image.load('covinv_docs/samus.png'), (
 
 # Charge L'image des boss
 BatbossImage = pygame.transform.scale(pygame.image.load(os.path.join('covinv_docs/pngegg.png')),
-                                      (WINDOW_WIDTH, WINDOW_HEIGHT))
+                                     (WINDOW_WIDTH, WINDOW_HEIGHT))
 
 # Charge L'image des objets
 
@@ -64,7 +64,7 @@ freezingImage = pygame.transform.scale(pygame.image.load('covinv_docs/freezing.p
 # Charge L'image de l'arrière plan
 
 StartBGImage = pygame.transform.scale(pygame.image.load(os.path.join('covinv_docs/phototest.jpg')),
-                                      (WINDOW_WIDTH, WINDOW_HEIGHT))
+                                     (WINDOW_WIDTH, WINDOW_HEIGHT))
 
 
 # MenuBGImage =  pygame.transform.scale(pygame.image.load(os.path.join('covinv_docs/pngegg.png')),(WINDOW_WIDTH,WINDOW_HEIGHT))
@@ -84,221 +84,264 @@ StartBGImage = pygame.transform.scale(pygame.image.load(os.path.join('covinv_doc
 # Story_SixBGImage =  pygame.transform.scale(pygame.image.load(os.path.join('covinv_docs/pngegg.png')),(WINDOW_WIDTH,WINDOW_HEIGHT))
 # Story_SevenBGImage =  pygame.transform.scale(pygame.image.load(os.path.join('covinv_docs/pngegg.png')),(WINDOW_WIDTH,WINDOW_HEIGHT))
 
-class Virus:
+class Falling:
+   def __init__(self, x, y):
+       self.x = x
+       self.y = y
+       self.falling_img = None
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.health = None
-        self.virus_img = None
+   def draw(self, window):
+       window.blit(self.falling_img, (self.x, self.y))
 
-    def draw(self, window):
-        window.blit(self.virus_img, (self.x, self.y))
+   def update(self):
+       pygame.event.pump()
 
-    def update(self):
-        pygame.event.pump()
+
+class Virus(Falling):
+
+   def __init__(self, x, y):
+       super().__init__(x, y)
+       self.health = None
+       self.virus_img = None
+
+   def draw(self, window):
+       window.blit(self.virus_img, (self.x, self.y))
+
+   def update(self):
+       super().update()
+
+   def move(self, vel):
+       self.y += vel
 
 
 class Bullet:
-    def __init__(self, x, y, img):
-        self.x = x
-        self.y = y
-        self.img = img
-        self.mask = pygame.mask.from_surface(self.img)
+   def __init__(self, x, y, img):
+       self.x = x
+       self.y = y
+       self.img = img
+       self.mask = pygame.mask.from_surface(self.img)
 
-    def draw(self, window):
-        window.blit(self.img, (self.x, self.y))
+   def draw(self, window):
+       window.blit(self.img, (self.x, self.y))
 
-    def move(self, vel):
-        self.y += vel
+   def move(self, vel):
+       self.y += vel
 
-    def off_screen(self, height):
-        return not (height >= self.y >= 0)
+   def off_screen(self, height):
+       return not (height >= self.y >= 0)
 
-    def collision(self, obj):
-        return collide(self, obj)
+   def collision(self, obj):
+       return collide(self, obj)
 
 
-class Hero:
+class Character:
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.health = None
-        self.hero_img = heroImage
-        self.mask = pygame.mask.from_surface(self.hero_img)
-        self.bullets = []
-        self.shoot_img = drop_img
+   def __init__(self, x, y):
+       self.x = x
+       self.y = y
+       self.health = None
+       self.character_img = None
+       self.mask = None
+       self.bullets = []
+       self.bullet_img = None
 
-    def draw(self, window):
-        WINDOW.blit(self.hero_img, (self.x, self.y))
-        for bullet in self.bullets:
-            bullet.draw(window)
+   #def shoot
+   #def move_bullets
 
-    def move_bullets(self, vel, objs):
-        for bullet in self.bullets:
-            bullet.move(vel)
-            if bullet.off_screen(WINDOW_HEIGHT):
-                self.bullets.remove(bullet)
-            else:
-                for obj in objs:
-                    if bullet.collision(obj):
-                        if obj.health - 1 == 0:
-                            objs.remove(obj)
-                        else:
-                            obj.health -= 1
-                        if bullet in self.bullets:
-                            self.bullets.remove(bullet)
+   #def draw
 
-    def update(self):
-        pygame.event.pump()
+   def update(self):
+       pygame.event.pump()
 
-    def shoot(self):
-        standard_ammo = Bullet(self.x + 8, self.y - 20, self.shoot_img)
-        self.bullets.append(standard_ammo)
+
+class Hero(Character):
+
+   def __init__(self, x, y):
+       super(Hero, self).__init__(x, y)
+       self.hero_img = heroImage
+       self.mask = pygame.mask.from_surface(self.hero_img)
+       self.bullet_img = drop_img
+
+   def draw(self, window):
+       window.blit(self.hero_img, (self.x, self.y))
+       for bullet in self.bullets:
+           bullet.draw(window)
+
+   def move_bullets(self, vel, objs):
+       for bullet in self.bullets:
+           bullet.move(vel)
+           if bullet.off_screen(WINDOW_HEIGHT):
+               self.bullets.remove(bullet)
+           else:
+               for obj in objs:
+                   if bullet.collision(obj):
+                       if obj.health - 1 == 0:
+                           objs.remove(obj)
+                       else:
+                           obj.health -= 1
+                       if bullet in self.bullets:
+                           self.bullets.remove(bullet)
+
+   def update(self):
+       super().update()
+
+   def shoot(self):
+       standard_ammo = Bullet(self.x + 8, self.y - 20, self.bullet_img)
+       self.bullets.append(standard_ammo)
 
 
 class Colorvirus(Virus):
-    Virus_MAP = {
-        "red": redVirusImage,
-        "green": greenVirusImage,
-        "blue": blueVirusImage,
-        "purple": purpleVirusImage
-    }
 
-    Health_Map = {
-        "red": HP_RED,
-        "green": HP_GREEN,
-        "blue": HP_BLUE,
-        "purple": HP_PURPLE
-    }
+   def __init__(self, x, y, color, hp):
+       super().__init__(x, y)
+       self.virus_img = self.Virus_MAP[color]
+       self.mask = pygame.mask.from_surface(self.virus_img)
+       self.health = self.Health_Map[hp]
 
-    def __init__(self, x, y, color, hp):
-        super().__init__(x, y)
-        self.virus_img = self.Virus_MAP[color]
-        self.mask = pygame.mask.from_surface(self.virus_img)
-        self.health = self.Health_Map[hp]
+   Virus_MAP = {
+       "red": redVirusImage,
+       "green": greenVirusImage,
+       "blue": blueVirusImage,
+       "purple": purpleVirusImage
+   }
 
-    def move(self, vel):
-        self.y += vel
+   Health_Map = {
+       "red": HP_RED,
+       "green": HP_GREEN,
+       "blue": HP_BLUE,
+       "purple": HP_PURPLE
+   }
 
 
-class Items(Virus):
-    Items_MAP = {
-        "mask": maskImage,
-        "vaccine": vaccineImage,
-        "ammo": bonusAmmoImage,
-        "trav_cert": travCertImage,
-        "freeze": freezingImage
-    }
+class Items(Falling):
+
+
+   Items_MAP = {
+       "mask": maskImage,
+       "vaccine": vaccineImage,
+       "ammo": bonusAmmoImage,
+       "trav_cert": travCertImage,
+       "freeze": freezingImage
+   }
+
+   def __init__(self, x, y):
+       super().__init__(x, y)
+       self.bonus_img = None
+
+   def draw(self, window):
+       window.blit(self.bonus_img, (self.x, self.y))
+
+   def update(self):
+       super().update()
 
 
 def collide(obj1, obj2):
-    offset_x = obj2.x - obj1.x
-    offset_y = obj2.y - obj1.y
-    return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None
+   diff_x = obj2.x - obj1.x
+   diff_y = obj2.y - obj1.y
+   return obj1.mask.overlap(obj2.mask, (diff_x, diff_y)) is not None
 
 
 def main():
-    run = True
-    level = 1
-    lives = 20
-    main_font = pygame.font.SysFont("timesnewroman", 20)
-    lost_font = pygame.font.SysFont("timesnewroman", 30, bold=True)
-    enemies = []
-    wave_length = 10
-    wave = 0
-    virus_vel = 1
-    shoot_vel = 5
+   run = True
+   level = 1
+   lives = 20
+   main_font = pygame.font.SysFont("timesnewroman", 20)
+   lost_font = pygame.font.SysFont("timesnewroman", 30, bold=True)
+   enemies = []
+   wave_length = 10
+   wave = 0
+   virus_vel = 1
+   bullet_vel = 5
 
-    hero = Hero(300, 500)
+   hero = Hero(300, 500)
 
-    clock = pygame.time.Clock()
-    lost = False
+   clock = pygame.time.Clock()
+   lost = False
 
-    def stop():
-        lost_label = lost_font.render("You have been infected", 1, (86, 189, 5))
-        lost_label2 = lost_font.render("You lost", 1, (86, 189, 5))
-        WINDOW.blit(lost_label, (WINDOW_WIDTH / 2 - lost_label.get_width() / 2, 260))
-        WINDOW.blit(lost_label2, (WINDOW_WIDTH / 2 - lost_label2.get_width() / 2, 300))
-        pygame.mixer.music.stop()
-        pygame.mixer.music.load('covinv_docs/Despi.mid')
-        pygame.mixer.music.play(-1, 0, 0)
-        while lost:
-            for event in pygame.event.get():
+   def stop():
+       lost_label = lost_font.render("You have been infected", 1, (86, 189, 5))
+       lost_label2 = lost_font.render("You lost", 1, (86, 189, 5))
+       WINDOW.blit(lost_label, (WINDOW_WIDTH / 2 - lost_label.get_width() / 2, 260))
+       WINDOW.blit(lost_label2, (WINDOW_WIDTH / 2 - lost_label2.get_width() / 2, 300))
+       pygame.mixer.music.stop()
+       pygame.mixer.music.load('covinv_docs/Despi.mid')
+       pygame.mixer.music.play(-1, 0, 0)
+       while lost:
+           for event in pygame.event.get():
 
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
+               if event.type == pygame.QUIT:
+                   pygame.quit()
+                   quit()
 
-            pygame.display.update()
-            clock.tick(15)
+           pygame.display.update()
+           clock.tick(15)
 
-    def redraw_window():
-        WINDOW.blit(StartBGImage, (0, 0))
-        WINDOW.blit(BatbossImage, (150, 0))
-        pygame.draw.line(WINDOW, (255, 0, 0), (0, 450), (600, 450), 3)
-        # draw text
-        lives_label = main_font.render(f"Lives: {lives}", 1, (255, 0, 255))
-        level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
-        wave_label = main_font.render(f"Wave: {wave}", 1, (255, 255, 255))
+   def redraw_window():
+       WINDOW.blit(StartBGImage, (0, 0))
+       WINDOW.blit(BatbossImage, (150, 0))
+       pygame.draw.line(WINDOW, (255, 0, 0), (0, 450), (600, 450), 3)
+       # draw text
+       lives_label = main_font.render(f"Lives: {lives}", 1, (255, 0, 255))
+       level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
+       wave_label = main_font.render(f"Wave: {wave}", 1, (255, 255, 255))
 
-        WINDOW.blit(lives_label, (10, 10))
-        WINDOW.blit(level_label, (WINDOW_WIDTH - level_label.get_width() - 10, 10))
-        WINDOW.blit(wave_label, (WINDOW_WIDTH / 2 - wave_label.get_width() / 2, 10))
+       WINDOW.blit(lives_label, (10, 10))
+       WINDOW.blit(level_label, (WINDOW_WIDTH - level_label.get_width() - 10, 10))
+       WINDOW.blit(wave_label, (WINDOW_WIDTH / 2 - wave_label.get_width() / 2, 10))
 
-        for enemy in enemies:
-            enemy.draw(WINDOW)
+       for enemy in enemies:
+           enemy.draw(WINDOW)
 
-        hero.draw(WINDOW)
-        pygame.display.update()
+       hero.draw(WINDOW)
+       pygame.display.update()
 
-    pygame.mixer.music.play(-1, 0, 0)
+   pygame.mixer.music.play(-1, 0, 0)
 
-    cooldown = 0
-    while run:
-        cooldown += 1
-        clock.tick(FPS)
-        if lives <= 0:
-            lost = True
-            stop()
-        if len(enemies) == 0:
-            wave += 1
-            wave_length += 5
-            for i in range(wave_length):
-                randVirus = random.choice(["red", "green", "blue", "purple"])
-                enemy = Colorvirus(random.randrange(50, WINDOW_WIDTH - 100), random.randrange(-1200, -300), randVirus,
-                                   randVirus)
-                enemies.append(enemy)
+   cooldown = 0
+   while run:
+       cooldown += 1
+       clock.tick(FPS)
+       if lives <= 0:
+           lost = True
+           stop()
+       if len(enemies) == 0:
+           wave += 1
+           wave_length += 5
+           for i in range(wave_length):
+               randVirus = random.choice(["red", "green", "blue", "purple"])
+               enemy = Colorvirus(random.randrange(50, WINDOW_WIDTH - 100), random.randrange(-1200, -300), randVirus,
+                                  randVirus)
+               enemies.append(enemy)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    run = False
-        if cooldown % 20 == 0:
-            hero.shoot()
+       for event in pygame.event.get():
+           if event.type == pygame.QUIT:
+               run = False
+           if event.type == KEYDOWN:
+               if event.key == K_ESCAPE:
+                   run = False
+       if cooldown % 20 == 0:
+           hero.shoot()
 
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and hero.x - 5 > 0:
-            hero.x -= 5
-        if keys[pygame.K_RIGHT] and hero.x + 5 + hero.hero_img.get_width() < WINDOW_WIDTH:
-            hero.x += 5
-        if keys[pygame.K_UP] and hero.y - 5 > 450:
-            hero.y -= 5
-        if keys[pygame.K_DOWN] and hero.y + 5 + hero.hero_img.get_height() < WINDOW_HEIGHT:
-            hero.y += 5
+       keys = pygame.key.get_pressed()
+       if keys[pygame.K_LEFT] and hero.x - 5 > 0:
+           hero.x -= 5
+       if keys[pygame.K_RIGHT] and hero.x + 5 + hero.hero_img.get_width() < WINDOW_WIDTH:
+           hero.x += 5
+       if keys[pygame.K_UP] and hero.y - 5 > 450:
+           hero.y -= 5
+       if keys[pygame.K_DOWN] and hero.y + 5 + hero.hero_img.get_height() < WINDOW_HEIGHT:
+           hero.y += 5
 
-        for enemy in enemies[:]:
-            enemy.move(virus_vel)
-            if enemy.y + enemy.virus_img.get_height() > WINDOW_HEIGHT - 150:
-                lives -= 1
-                enemies.remove(enemy)
+       for enemy in enemies[:]:
+           enemy.move(virus_vel)
+           if enemy.y + enemy.virus_img.get_height() > WINDOW_HEIGHT - 150:
+               lives -= 1
+               enemies.remove(enemy)
 
-        hero.move_bullets(-shoot_vel, enemies)
+       hero.move_bullets(-bullet_vel, enemies)
 
-        redraw_window()
+       redraw_window()
 
 
 main()
+
