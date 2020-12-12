3bd9df4f9f709c0ea3fce2620c2f81ec1ca03f93
remyzare <remy.nasser@unine.ch>
2020-12-12 01:24:48 +0100

    sound done

--- covinvaders.py
+++ covinvaders.py
@@ -1,15 +1,10 @@
-
 import pygame
 import random
 
 from pygame.locals import *
 
-
 pygame.font.init()
 pygame.mixer.init()
-
-
-
 
 FPS = 60
 WINDOW_HEIGHT = 600
@@ -17,12 +12,10 @@ WINDOW_WIDTH = 600
 WINDOW = pygame.display.set_mode((WINDOW_HEIGHT, WINDOW_WIDTH))
 pygame.display.set_caption("Convinvaders : Revenge of The Pangolin")
 
-
 redVirusImage = pygame.transform.scale(pygame.image.load('covinv_docs/red_virus.png'), (50, 50))
 greenVirusImage = pygame.transform.scale(pygame.image.load('covinv_docs/green_virus.png'), (50, 50))
 blueVirusImage = pygame.transform.scale(pygame.image.load('covinv_docs/blue_virus.png'), (50, 50))
 purpleVirusImage = pygame.transform.scale(pygame.image.load('covinv_docs/purple_virus.png'), (50, 50))
-
 
 drop_img = pygame.transform.scale(pygame.image.load('covinv_docs/drop.png'), (20, 20))
 pangbat_img = pygame.transform.scale(pygame.image.load('covinv_docs/missilelittlebat.png'), (80, 80))
@@ -47,18 +40,18 @@ batBossImage = pygame.transform.scale(pygame.image.load('covinv_docs/pngegg.png'
                                       (200, 140))
 
 bossUSImage = pygame.transform.scale(pygame.image.load('covinv_docs/BossUS.png'),
-                                      (200, 200))
+                                     (200, 200))
 
 angryBossUSImage = pygame.transform.scale(pygame.image.load('covinv_docs/ANGRY BOSS US.png'),
-                                      (200, 200))
+                                          (200, 200))
 
 pangolinImage = pygame.transform.scale(pygame.image.load('covinv_docs/pangolin.png'),
-                                      (200, 150))
+                                       (200, 150))
 
 pangolindefImage = pygame.transform.scale(pygame.image.load('covinv_docs/pangolin_en_boule.png'),
-                                      (160, 160))
-explosionImage =  pygame.transform.scale(pygame.image.load('covinv_docs/explosion.png'),
-                                      (200, 200))
+                                          (160, 160))
+explosionImage = pygame.transform.scale(pygame.image.load('covinv_docs/explosion.png'),
+                                        (200, 200))
 
 maskImage = pygame.transform.scale(pygame.image.load('covinv_docs/medical-mask.png'), (50, 50))
 vaccineImage = pygame.transform.scale(pygame.image.load('covinv_docs/syringe.png'), (50, 50))
@@ -66,44 +59,46 @@ bonusAmmoImage = pygame.transform.scale(pygame.image.load('covinv_docs/ammo.png'
 travCertImage = pygame.transform.scale(pygame.image.load('covinv_docs/certification.png'), (50, 50))
 freezingImage = pygame.transform.scale(pygame.image.load('covinv_docs/freezing.png'), (40, 40))
 
-
 startBGImage = pygame.transform.scale(pygame.image.load('covinv_docs/phototest.jpg'),
                                       (WINDOW_WIDTH, WINDOW_HEIGHT))
 jungle_BG = pygame.transform.scale(pygame.image.load('covinv_docs/test_BG.jpg'),
                                    (WINDOW_WIDTH, WINDOW_HEIGHT))
 batcave_BG = pygame.transform.scale(pygame.image.load('covinv_docs/bat_cave.jpeg'),
-                                   (WINDOW_WIDTH, WINDOW_HEIGHT))
+                                    (WINDOW_WIDTH, WINDOW_HEIGHT))
 DC_BG = pygame.transform.scale(pygame.image.load('covinv_docs/DC_landscape.jpeg'),
-                                   (WINDOW_WIDTH, WINDOW_HEIGHT))
+                               (WINDOW_WIDTH, WINDOW_HEIGHT))
 whitehouse_BG = pygame.transform.scale(pygame.image.load('covinv_docs/white_house_fire.jpg'),
-                                   (WINDOW_WIDTH, WINDOW_HEIGHT))
+                                       (WINDOW_WIDTH, WINDOW_HEIGHT))
 space_BG = pygame.transform.scale(pygame.image.load('covinv_docs/earth.jpg'),
-                                   (WINDOW_WIDTH, WINDOW_HEIGHT))
+                                  (WINDOW_WIDTH, WINDOW_HEIGHT))
 spacesun_BG = pygame.transform.scale(pygame.image.load('covinv_docs/sun_pango (2).jpg'),
-                                   (WINDOW_WIDTH, WINDOW_HEIGHT))
+                                     (WINDOW_WIDTH, WINDOW_HEIGHT))
 story1_img = pygame.transform.scale(pygame.image.load('covinv_docs/StartBG 1.jpg'),
-                                   (WINDOW_WIDTH, WINDOW_HEIGHT))
+                                    (WINDOW_WIDTH, WINDOW_HEIGHT))
 story2_img = pygame.transform.scale(pygame.image.load('covinv_docs/StartBG 2.jpg'),
-                                   (WINDOW_WIDTH, WINDOW_HEIGHT))
+                                    (WINDOW_WIDTH, WINDOW_HEIGHT))
 story3_img = pygame.transform.scale(pygame.image.load('covinv_docs/StartBG 3.jpg'),
-                                   (WINDOW_WIDTH, WINDOW_HEIGHT))
+                                    (WINDOW_WIDTH, WINDOW_HEIGHT))
 story4_img = pygame.transform.scale(pygame.image.load('covinv_docs/StartBG 4.jpg'),
-                                   (WINDOW_WIDTH, WINDOW_HEIGHT))
+                                    (WINDOW_WIDTH, WINDOW_HEIGHT))
 story5_img = pygame.transform.scale(pygame.image.load('covinv_docs/StartBG 5.jpg'),
-                                   (WINDOW_WIDTH, WINDOW_HEIGHT))
+                                    (WINDOW_WIDTH, WINDOW_HEIGHT))
 story6_img = pygame.transform.scale(pygame.image.load('covinv_docs/StartBG 6.jpg'),
-                                   (WINDOW_WIDTH, WINDOW_HEIGHT))
+                                    (WINDOW_WIDTH, WINDOW_HEIGHT))
 top1_img = pygame.transform.scale(pygame.image.load('covinv_docs/victory_royale.jpg'),
-                                   (WINDOW_WIDTH, WINDOW_HEIGHT))
-
+                                  (WINDOW_WIDTH, WINDOW_HEIGHT))
 
 ahSound = pygame.mixer.Sound('covinv_docs/scream.wav')
 waterSound = pygame.mixer.Sound('covinv_docs/water.wav')
+victorySound = pygame.mixer.Sound('covinv_docs/lvl_win.wav')
+boomSound = pygame.mixer.Sound('covinv_docs/boom.wav')
+
 
 HP_RED = 1
 HP_GREEN = 2
 HP_BLUE = 3
 HP_PURPLE = 4
+
 
 class Falling:
     def __init__(self, x, y):
@@ -142,7 +137,7 @@ class Bullet:
         self.y = y
         self.img = img
         self.mask = pygame.mask.from_surface(self.img)
-        self.random_direction = random.randrange(-8,8,1)
+        self.random_direction = random.randrange(-8, 8, 1)
 
     def draw(self, window):
         window.blit(self.img, (self.x, self.y))
@@ -175,6 +170,7 @@ class Character:
         self.mask = None
         self.bullets = []
         self.bullet_img = None
+
     def update(self):
         pygame.event.pump()
 
@@ -188,7 +184,7 @@ class Boss(Character):
         self.bullet_img = None
         self.bullets = []
         self.maxhealth = 50
-        #self.health = 50
+        # self.health = 50
         self.random_vel = random.choice([2, -2])
 
     def draw(self, window):
@@ -205,7 +201,7 @@ class Boss(Character):
             else:
                 if bullet.collides_with(hero):
                     if not invincible:
-                        if hero.lives  > 0:
+                        if hero.lives > 0:
                             hero.lives -= 1
                     if bullet in self.bullets:
                         self.bullets.remove(bullet)
@@ -218,10 +214,11 @@ class Boss(Character):
             else:
                 if bullet.collides_with(hero):
                     if not invincible:
-                        if hero.lives  >= 0:
+                        if hero.lives >= 0:
                             hero.lives -= 1
                     if bullet in self.bullets:
                         self.bullets.remove(bullet)
+
     def move_bullets_batBoss(self, vel, hero, invincible):
         for bullet in self.bullets:
             bullet.move(-vel)
@@ -240,11 +237,12 @@ class Boss(Character):
         pygame.draw.rect(window, (0, 255, 0), (self.x + 50, self.y, (100) * self.health / self.maxhealth, 8))
 
     def shoot_BossUS(self):
-        boss_ammo = Bullet(random.choice([self.x , self.x + 150]), self.y - 100 + self.boss_img.get_height(), self.bullet_img)
+        boss_ammo = Bullet(random.choice([self.x, self.x + 150]), self.y - 100 + self.boss_img.get_height(),
+                           self.bullet_img)
         self.bullets.append(boss_ammo)
 
     def shoot_Pangolin(self):
-        boss_ammo = Bullet(self.x + self.boss_img.get_width()/2 - 40, self.y + 100, self.bullet_img)
+        boss_ammo = Bullet(self.x + self.boss_img.get_width() / 2 - 40, self.y + 100, self.bullet_img)
         self.bullets.append(boss_ammo)
 
     def move(self, vel):
@@ -346,7 +344,6 @@ class Colorvirus(Virus):
 
 
 class Bonus(Falling):
-
     Bonus_List = {
         "mask": maskImage,
         "vaccine": vaccineImage,
@@ -371,7 +368,6 @@ def collide(obj1, obj2):
     return obj1.mask.overlap(obj2.mask, (diff_x, diff_y)) is not None
 
 
-
 def main(lvl, vague, hpbat, hpus, hppang):
     run = True
     level = lvl
@@ -393,13 +389,12 @@ def main(lvl, vague, hpbat, hpus, hppang):
     bullet_vel = 5
     boss_vel = 2
 
-
     hero = Hero(300, 500)
     batBoss = Boss(200, 0)
     batBoss.bullet_img = batredfire_img
     bossUS = Boss(200, 0)
     bossUS.bullet_img = dynamiteUS_img
-    pangolinBoss = Boss(200,0)
+    pangolinBoss = Boss(200, 0)
     pangolinBoss.bullet_img = pangbat_img
 
     batBoss.health = hpbat
@@ -510,8 +505,8 @@ def main(lvl, vague, hpbat, hpus, hppang):
             stop(level)
         if level == 0:
 
-            pygame.mixer.music.stop()
-
+            pygame.mixer.music.load('covinv_docs/txt_screen.mp3')
+            pygame.mixer.music.play(-1, 0, 0)
             animation = text_screen(level, story1_img, explosionImage, -300, -300)
             level += 1
 
@@ -569,7 +564,8 @@ def main(lvl, vague, hpbat, hpus, hppang):
             timer_freeze -= 1
             if wave == 6:
                 pygame.mixer.music.stop()
-
+                pygame.mixer.music.load('covinv_docs/txt_screen.mp3')
+                pygame.mixer.music.play(-1, 0, 0)
                 animation = text_screen(level, story2_img, jungle_BG, -300, -300)
                 wave += 1
             if wave == 7:
@@ -604,7 +600,7 @@ def main(lvl, vague, hpbat, hpus, hppang):
                     minibat = Colorvirus(random.randrange(50, WINDOW_WIDTH - 100), -100, "minibat", "minibat")
                     enemies.append(minibat)
             if boss_cooldown % 650 == 0:
-                randBonus = random.choice(["ammo","trav_cert", "vaccine","mask"])
+                randBonus = random.choice(["ammo", "trav_cert", "vaccine", "mask"])
                 bonus = Bonus(random.randrange(50, WINDOW_WIDTH - 100), -100, randBonus)
                 bonuses.append(bonus)
             batBoss.move_bullets_batBoss(-bullet_vel, hero, invincible)
@@ -618,7 +614,6 @@ def main(lvl, vague, hpbat, hpus, hppang):
                     if not invincible:
                         hero.lives -= 1
                     enemies.remove(minibat)
-
 
             for bonus in bonuses[:]:
                 bonus.move(bonus_vel)
@@ -635,12 +630,16 @@ def main(lvl, vague, hpbat, hpus, hppang):
                         timer_mask = 300
 
             if batBoss.health == 0:
-                ahSound.play()
                 pygame.mixer.music.stop()
+                boomSound.play()
+                ahSound.play()
+                victorySound.play()
+                pygame.mixer.music.load('covinv_docs/txt_screen.mp3')
+                pygame.mixer.music.play(-1, 0, 0)
                 animation = text_screen(level, story3_img, BG, batBoss.x, batBoss.y)
                 batBoss.health = -1
             if batBoss.health == -1:
-                level +=1
+                level += 1
                 pygame.mixer.music.load('covinv_docs/jojo.mp3')
                 pygame.mixer.music.play(-1, 0, 0)
 
@@ -706,7 +705,8 @@ def main(lvl, vague, hpbat, hpus, hppang):
             timer_freeze -= 1
             if wave == 6:
                 pygame.mixer.music.stop()
-
+                pygame.mixer.music.load('covinv_docs/txt_screen.mp3')
+                pygame.mixer.music.play(-1, 0, 0)
                 animation = text_screen(level, story4_img, BG, -300, -300)
                 wave += 1
             if wave == 7:
@@ -741,7 +741,7 @@ def main(lvl, vague, hpbat, hpus, hppang):
             if boss_cooldown % 60 == 0:
                 bossUS.y += 1
             if boss_cooldown % 650 == 0:
-                randBonus = random.choice(["ammo","trav_cert", "vaccine","mask"])
+                randBonus = random.choice(["ammo", "trav_cert", "vaccine", "mask"])
                 bonus = Bonus(random.randrange(50, WINDOW_WIDTH - 100), -100, randBonus)
                 bonuses.append(bonus)
             bossUS.move_bullets_BossUS(-bullet_vel, hero, invincible)
@@ -762,10 +762,12 @@ def main(lvl, vague, hpbat, hpus, hppang):
                         timer_mask = 300
 
             if bossUS.health == 0:
-                ahSound.play()
-
                 pygame.mixer.music.stop()
-
+                boomSound.play()
+                ahSound.play()
+                victorySound.play()
+                pygame.mixer.music.load('covinv_docs/txt_screen.mp3')
+                pygame.mixer.music.play(-1, 0, 0)
                 animation = text_screen(level, story5_img, BG, bossUS.x, bossUS.y)
                 bossUS.health = -1
             if bossUS.health == -1:
@@ -834,14 +836,14 @@ def main(lvl, vague, hpbat, hpus, hppang):
             timer_freeze -= 1
             if wave == 6:
                 pygame.mixer.music.stop()
-
+                pygame.mixer.music.load('covinv_docs/txt_screen.mp3')
+                pygame.mixer.music.play(-1, 0, 0)
                 animation = text_screen(level, story6_img, BG, -300, -300)
                 wave += 1
             if wave == 7:
                 level += 1
                 pygame.mixer.music.load('covinv_docs/ofortuna.mp3')
                 pygame.mixer.music.play(-1, 0, 0)
-
 
                 enemies = []
                 bonuses.clear()
@@ -860,7 +862,7 @@ def main(lvl, vague, hpbat, hpus, hppang):
             if timer_pangolin <= 400:
                 timer_pangolin += 1
                 pangolinBoss.boss_img = pangolinImage
-                pangolinBoss.move(boss_vel*2)
+                pangolinBoss.move(boss_vel * 2)
                 if boss_cooldown % 70 == 0:
                     pangolinBoss.shoot_Pangolin()
             else:
@@ -873,7 +875,7 @@ def main(lvl, vague, hpbat, hpus, hppang):
                     timer_pangolin_def = 0
 
             if boss_cooldown % 650 == 0:
-                randBonus = random.choice(["ammo","trav_cert", "vaccine","mask"])
+                randBonus = random.choice(["ammo", "trav_cert", "vaccine", "mask"])
                 bonus = Bonus(random.randrange(50, WINDOW_WIDTH - 100), -100, randBonus)
                 bonuses.append(bonus)
             pangolinBoss.move_bullets_pangolin(-bullet_vel, hero, invincible)
@@ -894,9 +896,18 @@ def main(lvl, vague, hpbat, hpus, hppang):
                         timer_mask = 300
 
             if pangolinBoss.health <= 0:
+                pygame.mixer.music.stop()
+                boomSound.play()
                 ahSound.play()
+                victorySound.play()
+                pygame.mixer.music.load('covinv_docs/txt_screen.mp3')
+                pygame.mixer.music.play(-1, 0, 0)
                 animation = text_screen(level, top1_img, BG, pangolinBoss.x, pangolinBoss.y)
+                level += 1
+                pygame.mixer.music.stop()
+                victorySound.play()
         if level == 7:
+            victorySound.play()
             final_screen()
 
         for event in pygame.event.get():
@@ -926,7 +937,8 @@ def main(lvl, vague, hpbat, hpus, hppang):
 
         redraw_window()
 
-def  pangolin_arriving():
+
+def pangolin_arriving():
     timer_pangolin_arriving = 16320
     run = True
     i = -700
@@ -954,14 +966,15 @@ def text_screen(lvl, image, BG, x, y):
         timer_explosion = 1000
     run = True
     clear_font = pygame.font.SysFont("timesnewroman", 30, bold=True)
-    clear_label = clear_font.render("Niveau terminé", 1, (255,255,255))
+    clear_label = clear_font.render("Niveau terminé", 1, (255, 255, 255))
     mac_font = pygame.font.SysFont("timesnewroman", 20, bold=True)
     mac_label = mac_font.render("Si vous jouez sur un mac, pressez la barre espace", 1, (255, 255, 255))
+
     while run:
         while timer_explosion > 0:
-            WINDOW.blit(BG, (0,0))
-            WINDOW.blit(explosionImage, (x,y))
-            WINDOW.blit(herowin,(225,320))
+            WINDOW.blit(BG, (0, 0))
+            WINDOW.blit(explosionImage, (x, y))
+            WINDOW.blit(herowin, (225, 320))
             WINDOW.blit(clear_label, (WINDOW_WIDTH / 2 - clear_label.get_width() / 2, 280))
             pygame.display.update()
             timer_explosion -= 1
@@ -985,12 +998,14 @@ def text_screen(lvl, image, BG, x, y):
                     return animation
     pygame.quit()
 
+
 def final_screen():
     run = True
     final_font = pygame.font.SysFont("timesnewroman", 20, bold=True)
     final_label = final_font.render("BON TRAVAIL ! TU AS MIS FIN A L'INVASION DU COVID", 1, (255, 255, 255))
-    continue_label = final_font.render("SI TU VEUX RECOMMENCER APPUIE SUR ESPACE", 1, (255,255,255))
+    continue_label = final_font.render("SI TU VEUX RECOMMENCER APPUIE SUR ESPACE", 1, (255, 255, 255))
     pygame.mixer.music.stop()
+    victorySound.play()
     while run:
 
         WINDOW.blit(startBGImage, (0, 0))
@@ -1009,6 +1024,7 @@ def final_screen():
 
     pygame.quit()
 
+
 def main_start():
     pygame.init()
     run = True
@@ -1026,9 +1042,8 @@ def main_start():
                 if event.key == K_ESCAPE:
                     run = False
                 else:
-                    main(0, 0,50, 50, 50)
+                    main(0, 0, 50, 50, 50)
     pygame.quit()
 
 
 main_start()
-
